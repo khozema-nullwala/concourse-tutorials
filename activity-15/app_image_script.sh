ec1d2db7c46a397e434fd810b3e2486ef1a05fbf
Khozema Nullwala <khozema.nullwala@accenture.com>
2020-09-24 15:55:49 +0530

    Fixed Activity-15!

--- activity-15/app_image_script.sh
+++ activity-15/app_image_script.sh
@@ -4,5 +4,5 @@ output_dir=workspace
 mvn -Dmaven.repo.local=/app/cache -f $(pwd)/simple-java-app/pom.xml test
 mvn -Dmaven.repo.local=/app/cache -f $(pwd)/simple-java-app/pom.xml jar:jar
 mv $(pwd)/simple-java-app/target/*.jar "${output_dir}/"
-cp $(pwd)/concourse-tutorials/lab-5.1/docker/AppDockerfile "${output_dir}/Dockerfile"
+cp $(pwd)/concourse-tutorials/activity-15/docker/AppDockerfile "${output_dir}/Dockerfile"
 cp $(pwd)/version/number "${output_dir}/number"
--- activity-15/cache_image_script.sh
+++ activity-15/cache_image_script.sh
@@ -3,4 +3,4 @@ set +ex
 output_dir=workspace
 mvn -Dmaven.repo.local=$(pwd)/simple-java-app/cache -DskipTests -f $(pwd)/simple-java-app/pom.xml package
 mv $(pwd)/simple-java-app/cache "${output_dir}/"
-cp $(pwd)/concourse-tutorials/lab-5.1/docker/CacheDockerfile "${output_dir}/Dockerfile"
+cp $(pwd)/concourse-tutorials/activity-15/docker/CacheDockerfile "${output_dir}/Dockerfile"
